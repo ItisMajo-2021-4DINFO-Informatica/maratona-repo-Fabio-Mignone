c11130a3d00b5247e85a68a82bc020d12011c3cd
Fabio Mignone <mignofax@gmail.com>
2022-02-16 13:51:07 +0100

    Progetto caricato
    progetto caricato con lista e classe dei metodi

---
+++ MignoneMaratona/MignoneMaratona/Maratona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MignoneMaratona
+{
+    class Maratona
+    {
+        public string NomeAtleta { get; set; }
+        public string Società { get; set; }
+        public int TempoImpiegato { get; set; }
+        public string CittàMaratona { get; set; }
+    }
+}
---
+++ MignoneMaratona/MignoneMaratona/Maratone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MignoneMaratona
+{
+    class Maratone
+    {
+        public List<Maratona> ElencoMaratona;
+
+        public Maratone()
+        {
+            ElencoMaratona = new List<Maratona>();
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
